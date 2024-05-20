9da6aa45e91da291e0ec5ca1f5508b652ed1193a
Aldo <aeveraespinoza@mail.lipscomb.edu>
2024-05-19 20:08:42 -0500

    finished cmd option checking; finished the pekora img output

--- stamper.cpp
+++ stamper.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 enum class State {
     IMAGE    = 0,
@@ -7,39 +8,91 @@ enum class State {
     FAIL     = 2, 
 };
 
+//Shows appropriate usage of the program.
 inline void help() noexcept {
-    const std::string_view usage{
-        "\nUsage:\t./stamper.out [Option...]\n\n"
+    std::string_view usage{
+        "\nUsage:\t./stamper.out [Option...] <CommentStr>\n\n"
     };
-    const std::string_view noImageOption{
+    std::string_view noImageOption{
         "\t-n\t\tOutputs only the box comment.\n"
     };
-    const std::string_view helpOption   {
+    std::string_view helpOption   {
         "\t-h or --help    Outputs usage message.\n"
     };
     std::cout << usage << noImageOption << helpOption << std::endl;
     return;
 }
 
-inline void middleGround() noexcept {
-    std::cout <<
-        "__   _____  _   _ ____     ____ ___  ____  _____\n"        << 
-        "\\ \\ / / _ \\| | | |  _ \\   / ___/ _ \\|  _ \\| ____|\n" <<
-        " \\ V / | | | | | | |_) | | |  | | | | | | |  _|\n"        <<        
-        "  | || |_| | |_| |  _ <  | |__| |_| | |_| | |___\n"        << 
-        "  |_| \\___/ \\___/|_| \\_\\  \\____\\___/|____/|_____|\n" <<
-    std::endl;
+//Prints a simple box comment. The name is hard coded, but
+//it can be changed for anyone else.
+inline void boxComment(std::string_view comment) noexcept{
+    std::cout << comment;
+    return;
+}
+
+//Prints an image of smug Pekora.
+inline void image(std::string_view comment) noexcept {
+    //Hard coded because I do not want another file to keep track of.
+    std::vector<const char*> image {
+        "⠀⠀⠀⠀⠀⡆⠀⠀⠀⢲⠀⠀⠹⡄⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⡞⠀⠐⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠸⡀⠀⠀⠀⢣⡀⠀⠱⡄⠀⠀⠀⠀⠀⠀⠀⢸⡓⠛⠳⠺⣇⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠑⢄⠀⠀⢀⣷⣄⡀⠘⢦⣀⣠⡴⠶⠚⠛⠛⠃⠒⠒⠒⠚⠦⢀⣻⡉⠉⠉⠑⠒⠢⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⣶⢒⢲⡄⠀⠙⠺⣯⣡⢤⡚⠋⠉⠉⠐⠒⠉⠉⠓⠒⠢⠄⣀⠀⠀⠀⠀⠈⠑⠦⡀⠀⠀⠀⠀⠀⠉⠒⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⠈⠣⣀⡷⠀⣠⣾⠟⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠂⢀⠀⠀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠈⠑⢄⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠁⢠⣾⠏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠒⢄⠀⠀⠀⠑⢆⠀⠀⠀⠀⠀⠀⠀⠙⢄⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⡰⡷⠁⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⠀⠀⠀⠳⡀⠀⠀⠀⠀⠀⠀⠈⠳⡀⠀⠀",
+        "⠀⠀⠀⢀⣼⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⠀⠑⡄⠀",
+        "⠀⠀⢀⣞⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣄⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⠀⠘⣄",
+        "⠀⠀⡜⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢆⠀⠀⢳⡀⠀⠀⠀⠀⠀⠀⠠⡸",
+        "⠀⣰⠱⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠈⣷⠀⠀⠀⠀⠀⠀⠀⠘",
+        "⢠⠇⡆⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⢹⡀⠀⢹⡆⠀⠀⠀⠀⠀⢄⠀",
+        "⡼⢰⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⣿⡀⡄⠀⠀⠀⢠⣀⠀⡏⢣⡀⠀⠀⠀⠀⢳⡀⠀⠀⠀⠸⡄⠀⠀⠀⠀⣇⠀⠀⣷⠀⠀⠀⠀⠀⠀⠙",
+        "⠣⡇⠀⠀⠀⢠⡇⠀⠀⠀⢰⠂⠀⡏⣇⣷⠀⠀⠀⠈⢯⠉⢻⠒⠳⡄⠀⠀⠀⠀⢳⠀⠀⠀⠀⣇⠀⠀⠀⠀⢸⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀",
+        "⠀⠃⠀⠀⠀⢸⠇⠀⠀⠀⢸⠀⢸⡧⢏⢹⡀⠀⠀⠀⠀⠑⠺⡇⠊⠘⢢⠀⠀⠀⠈⣧⡀⠀⠀⢸⠀⠀⠀⠀⢸⡅⠀⢸⡇⠀⠀⠀⠀⠀⠀",
+        "⠠⠀⠀⠀⠀⢸⠇⠀⠀⠀⣾⠀⡿⠄⢼⠾⢧⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠑⢤⡀⠀⠸⡳⡀⠀⢸⠀⠀⠀⠀⢸⡄⠀⠈⣷⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⡀⠀⠸⡇⠀⠀⢀⡗⣰⠁⠀⠀⢣⡌⢢⡀⠀⠀⠀⠀⡇⠀⣰⣂⣀⣀⣭⣶⣤⣷⣿⣄⠘⡆⠀⠀⠀⢸⠆⠀⠀⣿⣤⣤⣤⢶⠾⠷",
+        "⠀⠀⠰⣧⠀⠀⣇⠀⠀⣼⣷⠃⠀⠀⣀⣠⣿⡀⠑⢄⡀⠀⠀⡇⠀⠙⠿⣿⣿⡿⣿⣿⣿⡆⠈⢲⡇⠀⠀⠀⣼⠀⠀⠀⣿⣧⣐⣤⣮⣼⣷",
+        "⢸⠀⠀⣿⡀⠀⢿⠀⣰⣿⣧⣶⣶⣿⣿⣯⠀⠁⠀⠀⠈⠢⢄⡇⠀⠀⢰⡾⠟⠷⠟⠛⣻⠃⠀⢰⠃⠀⠀⣰⡇⠀⠀⠀⣏⣿⡿⢟⢻⣿⣋",
+        "⢸⡆⠀⢿⣧⠀⢸⣦⠟⠛⠛⣿⣏⣻⠿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠒⠒⠋⠁⠀⠀⡸⠀⢀⣴⡟⠀⠀⠀⠀⣿⡟⣡⡼⠋⣠⠟",
+        "⠨⣇⠀⠘⣿⣦⠀⢿⡀⠀⠺⣟⣉⣉⠴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⢰⡅⠔⠋⣿⠀⠀⠀⠀⠀⣿⡼⠋⠀⢸⣁⠀",
+        "⡇⢹⡀⠀⢻⣿⣧⡈⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠛⠛⢦⠀⠀⠀⠀⠀⠀⠀⣟⠀⠀⠀⠀⠀⡿⠛⠢⣄⠀⠉⣿",
+        "⣷⠀⢧⠀⠘⣿⡸⢿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠴⡪⠃⠀⠀⠈⡇⠀⠀⠀⠀⢀⣴⣯⠀⠀⠀⠀⣸⡇⠀⠀⠈⢣⢸⠇",
+        "⣿⡄⠈⣧⡀⢸⡇⣹⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡦⠴⠖⠮⠭⠒⠉⠀⠀⠀⢀⡼⠁⠀⠀⣠⣴⣿⡛⡧⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠼⣇", 
+        "⠙⢳⡀⢸⡙⠲⢷⣸⣿⡷⠦⣀⡀⠀⠀⠀⠀⠀⠀⠳⢄⡀⠀⠀⠀⣀⣀⡠⠔⠋⣀⣠⡴⠛⠉⠀⠀⠉⡧⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠠⡟",
+        "⠀⠀⠁⠘⣇⠀⠀⠙⣿⡅⢠⡟⠉⠙⠓⠒⠦⠤⠤⠤⣤⡬⣭⣭⣭⠬⠴⡒⢚⡿⠛⠉⠀⠀⠀⠀⠀⠀⡗⠀⠀⠀⢸⡏⠀⠀⠀⠀⠀⠀⠘",
+    };
+
+    for(const auto & i: image){
+        std::cout << comment << i << std::endl;
+    }
+
     return;
 }
 
 int main(int argc, char* argv[]) {
-    State nextStep{State::IMAGE};
+    State nextStep {State::IMAGE};
+    std::string comment {"#"};
 
-    if (argc > 1)
-        nextStep = (argc == 2 && static_cast<std::string>(argv[1]) == "-n") ? State::NO_IMAGE : State::FAIL;    
-    
-    if (nextStep != State::FAIL)
-        return 0;
+    //Resolves what flags or input was given to the program.
+    if (argc > 3) nextStep = State::FAIL;
+
+    for(int i {1}; nextStep != State::FAIL && i < argc; ++i){
+        std::string_view cmdOption {static_cast<std::string_view>(argv[i])};
+        if (cmdOption.compare("-n") == 0)
+            nextStep = State::NO_IMAGE;
+        else
+            comment = cmdOption;
+    }
+
+    //Decides how to format the output.
+    if(nextStep != State::FAIL){
+        boxComment(comment);
+        std::cout << "\n\n" << std::endl;
+        if (nextStep == State::IMAGE) {
+            image(comment);
+            std::cout << std::endl;
+        }
+    }
     else
         help();
 
