b948e1217fd92e67168ff0aaa941185d3ad5cdb4
Aldo <aeveraespinoza@mail.lipscomb.edu>
2024-05-25 20:29:00 -0500

    added documentation

--- c/main.c
+++ c/main.c
@@ -1,3 +1,13 @@
+//-----------------------------------------------------------------//
+//  Developer: Aldo Vera-Espinoza                  ___     _______ //
+//  Date: Sat May 25 20:27:51 2024                / \ \   / / ____|//
+//                                               / _ \ \ / /|  _|  //
+//  Project Name: Clox                          / ___ \ V / | |___ //
+//  Description:                               /_/   \_\_/  |_____|//
+//    This is the main program for the C version                   //
+//  of an interpreter for lox.                                     //
+//-----------------------------------------------------------------//
+
 #include "common.h"
 
 int main(int argc, const char* argv[]){
--- go/loxInterpreter.go
+++ go/loxInterpreter.go
@@ -1,3 +1,13 @@
+//-----------------------------------------------------------------//
+//  Developer: Aldo Vera-Espinoza                  ___     _______ //
+//  Date: Sat May 25 20:24:19 2024                / \ \   / / ____|//
+//                                               / _ \ \ / /|  _|  //
+//  Project Name: GOlox                         / ___ \ V / | |___ //
+//  Description:                               /_/   \_\_/  |_____|//
+//    This is the main program for the Go version                  //
+//  of an interpreter for Lox.                                     //
+//-----------------------------------------------------------------//
+
 package main
 
 import "fmt"
